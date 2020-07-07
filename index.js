5dc149cad6dff7cec3393faefe2e28e2d97c2b27
Serge M <serge@tommyjohn.com>
2020-07-07 18:54:21 -0400

    Adding router

--- index.js
+++ index.js
@@ -6,10 +6,31 @@ dotenv.config({path: './config/config.env'});
 const app = express();
 const PORT  = process.env.PORT || 5000;
 
+// Routes 
+const items = require('./routes/items')
+const groups = require('./routes/groups')
+
+app.use('/api/v1/items', items)
+app.use('/api/v1/groups', groups)
+
 app.get('/', (req, res) => {
   res.send('eh... whats up doc!')
 });
-app.get('/api/item-groups', (req, res) => {
-  res.send()
+
+app.get('/api/v1', (req, res) => {
+  res.status(200).json({
+    "success": true,
+    "msg": "received Successfully",
+    "data": {
+    }
+  })
+})
+
+app.patch('/api/v1/item-groups', (req, res) => {
+  res.status(200).json({
+    "success": true, 
+    "data": {
+    }
+  })
 })
 app.listen(PORT, ()=> console.log(`running on ${process.env.NODE_ENV} mode! Listening on port: ${PORT}`));
---
+++ routes/items.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router()
+
+
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    "success": true,
+    "msg": `received Query Successfully ${req.query.query}`,
+    "data": [
+      {
+        "id": 5576,
+        "title": "Product A",
+        "groups": [
+          { "id": 17, "title": "Group A" }
+        ]
+      }
+    ]
+  })
+})
+
+router.get('/:id', (req, res) => {
+  res.status(200).json({
+    "success": true,
+    "msg": `received Query Successfully ${req.query}`,
+    "data": {
+      "id": 5576,
+      "title": "Product A",
+      "handle": "product_a",
+      "groups": [
+        { "id": 17, "title": "Group A" }
+      ]
+    }
+  })
+})
+
+router.get('/:id/groups', (req, res) => {
+  res.status(200).json({
+    "success": true,
+    "msg": `received Query Successfully ${req.query}`,
+    "data": {
+      "id": 5576,
+      "title": "Product A",
+      "handle": "product_a",
+      "groups": [
+        { "id": 17, "title": "Group A" }
+      ]
+    }
+  })
+})
+
+module.exports = router
